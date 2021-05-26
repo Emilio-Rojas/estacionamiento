43ad34af7709cd25d5f6bf73dbeb2ed1e32b6299
EmilioRC <emilio.r.cruz@gmail.com>
2021-05-26 01:11:31 -0400

    Primer avance home

--- src/mi_estacionamiento/settings.py
+++ src/mi_estacionamiento/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mi_estacionamiento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'usuarios.User'
 
