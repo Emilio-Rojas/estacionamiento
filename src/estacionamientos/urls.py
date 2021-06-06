76d0fe963610b1e90c3e52778ede6e50cf3c19a7
EmilioRC <emilio.r.cruz@gmail.com>
2021-06-06 17:48:23 -0400

    Modulo completo estacionamiento particular

--- src/estacionamientos/urls.py
+++ src/estacionamientos/urls.py
@@ -8,6 +8,14 @@ from .views import *
 
 urlpatterns = [
     path('administrar/', administrar_estacionamiento, name='administrar-estacionamiento'),
+    path('disponibilidad/', disponibilidad_estacionamiento, name='disponibilidad-estacionamiento'),
     path('privado/', estacionamiento_privado, name='estacionamiento-privado'),
-    path('particular/', estacionamiento_particular, name='estacionamiento-particular'),
+
+    #Estacionamiento particulares
+    path('particular/', include([
+        path('agregar/', estacionamiento_particular_agregar, name='estacionamiento-particular-agregar'),
+        path('listar/', estacionamiento_particular_listar, name='estacionamiento-particular-listar'),
+        path('editar/<int:id_estacionamiento>/', estacionamiento_particular_editar, name='estacionamiento-particular-editar'),
+        path('eliminar/<int:id_estacionamiento>/', estacionamiento_particular_eliminar, name='estacionamiento-particular-eliminar')
+    ])),
 ]
--- src/estacionamientos/views.py
+++ src/estacionamientos/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render, redirect
+from estacionamientos.forms import AgregarHabitacionForms, EditarHabitacionForms
+
+from estacionamientos.models import Estacionamiento
 
 def administrar_estacionamiento(request):
     return render(request, 'estacionamientos/administrar_estacionamiento.html', {})
 
-
 def estacionamiento_privado(request):
     return render(request, 'estacionamientos/estacionamiento_privado.html', {})
 
-def estacionamiento_particular(request):
-    return render(request, 'estacionamientos/estacionamiento_particular.html', {})
+def disponibilidad_estacionamiento(request):
+    return render(request, 'estacionamientos/disponibilidad_estacionamiento.html', {})
+
+
+
+#CRUD ESTACIONAMIENTO PARTICULAR
+
+def estacionamiento_particular_listar(request):
+    estacionamientos = Estacionamiento.objects.all().order_by('id')
+    return render(request, 'estacionamientos/estacionamiento_particular_listar.html', {'estacionamientos': estacionamientos})
+
+def estacionamiento_particular_agregar(request):
+    if request.method == 'POST':
+        form = AgregarHabitacionForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('/estacionamiento/particular/listar')
+    else:
+        form = AgregarHabitacionForms()
+    return render(request, 'estacionamientos/estacionamiento_particular_agregar.html',{'form' : form})
+
+def estacionamiento_particular_editar(request, id_estacionamiento):
+    estacionamiento = Estacionamiento.objects.get(id=id_estacionamiento)
+    if request.method == "GET":
+        form = EditarHabitacionForms(instance=estacionamiento)
+    else:
+        form = EditarHabitacionForms(request.POST, request.FILES,instance=estacionamiento)
+        if form.is_valid():
+            estacionamiento = form.save(commit=False)
+            estacionamiento.save()
+        return redirect('/estacionamiento/particular/listar')
+    return render(request, "estacionamientos/estacionamiento_particular_editar.html", {'form': form})
+
+def estacionamiento_particular_eliminar(request, id_estacionamiento):
+    estacionamiento = Estacionamiento.objects.get(id=id_estacionamiento)
+    if request.method == 'POST':
+        estacionamiento.delete()
+        return redirect('/estacionamiento/particular/listar')
+    return render(request, 'estacionamientos/estacionamiento_particular_eliminar.html', {'estacionamiento': estacionamiento})
